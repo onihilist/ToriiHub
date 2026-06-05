da4c254b8ddbe2f1aadc524432e41a7026b69991
onihilist <mashirooo.yt@gmail.com>
2026-06-05 15:42:27 +0200

    feat(frontend): add React frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './assets/small-toriihub.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="toriihub-navbar">
+          <img src={logo} className="toriihub-navbar-logo" alt="logo"/>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
